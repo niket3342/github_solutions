af65cbd6b8bc222d9c8210d63077b4ceba9d7a57
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-06-07 23:13:14 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (72.65%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int target) 
+    int searchInsert(vector<int>& nums, int target)
     {
-        int n = nums.size();
-        int ans;
-        for(int i=0; i<n; i++)
+        ios_base :: sync_with_stdio(false);
+        cin.tie(NULL);
+        int low = 0;
+        int high = nums.size()-1;
+        int mid;
+        while(low<=high)
         {
-            if(nums[i] == target) return i;
-            else if(nums[i] > target)
-            {
-             return i;   
-            }
-            else continue;
+            mid = low + (high - low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low =mid+1;
+            else high = mid-1;
         }
-        return n;
-        
+        return low;
     }
 };
