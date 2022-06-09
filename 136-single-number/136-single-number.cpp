8d5c190ab6ad0efdb3c9c39fa051a2014b928b0c
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-06-09 20:38:20 +0530

    Time: 28 ms (53.76%), Space: 16.9 MB (53.30%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> m;
-        for(auto a:nums)m[a]++;//select numbers from nums and store them and increment the count corresponding to those numbers
-        for(auto a:m) if (a.second !=2) return a.first;
-        return -1;
+        int ans = 0;
+        for(auto a:nums) ans = ans^a;
+        return ans;
         
     }
 };
