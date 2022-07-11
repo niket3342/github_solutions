444010facdba00920433e960d43272a399c6994d
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-07-12 00:28:20 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (91.87%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
-    void func(vector<int> &ds,vector<int>& nums,vector<vector<int>>&ans,int freq[])
+    void func(int index,vector<int>& nums,vector<vector<int>>&ans)
     {
-        if(ds.size() == nums.size())
+        if(index == nums.size())
         {
-            ans.push_back(ds);
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=index;i<nums.size();i++)
         {
-            if(!freq[i])
-            {
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                func(ds,nums,ans,freq);
-                freq[i] = 0;
-                ds.pop_back();
-            }
+            swap(nums[index], nums[i]);
+            func(index+1, nums, ans);
+            swap(nums[index], nums[i]);
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) 
     {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        //the above freq array can be initialized using for loop too
-        for(int i=0;i<nums.size();i++) freq[i]=0;
-        func(ds,nums,ans,freq);
+        
+        func(0,nums,ans);
         return ans;
     }
 };
