4fbc0df3612cc2704d9c951b637e7edb52714ec5
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-08-04 17:50:02 +0530

    Time: 240 ms (25.01%), Space: 13.2 MB (82.61%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,27 +1,51 @@
 class Solution {
+
 public:
+    static bool compare(string &s1, string &s2)
+    {
+        if(s1.size() != s2.size()+1) return false;
+        
+        int first = 0;
+        int second = 0;
+        
+        while(first < s1.size())
+        {
+            if(second < s2.size() && s1[first] == s2[second])
+            {
+                first++;
+                second++;
+            }
+            else first++;            
+        }
+        if (first == s1.size() && second ==s2.size()) return true;
+        else return false;
+    }
+     static bool comp(string &s1, string &s2)
+    {
+        return s1.size() < s2.size();
+    }
+   
+    
+    
     int longestStrChain(vector<string>& words) 
     {
-        unordered_map<string, int> dp;
-        int ans =0 ;
+        int n = words.size();
+        sort(words.begin(),words.end(), comp);
+        vector<int> dp(n,1);
+        int maxi=1;
         
-        sort(words.begin(), words.end(), sortByLength);
-        for(string word : words)
+        for(int i=0;i<n;i++)
         {
-            for(int i=0; i<word.length();i++)
+            for(int prevInd = 0;prevInd<i;prevInd++)
             {
-            //remove characters one by one from the word and check if the obtained string is a predecessor
-            string predecessor = word.substr(0,i) + word.substr(i+1);
-            dp[word] = max(dp[word], dp[predecessor]+1);
+                if(compare(words[i], words[prevInd]) && 1+dp[prevInd] > dp[i])
+                {
+                    dp[i] = 1+dp[prevInd];
+                }
             }
-            ans = max(dp[word], ans);
+            if(dp[i]>maxi) maxi = dp[i];
         }
-        return ans;
+        return maxi;
         
-    }
-    
-    static bool sortByLength(string& s1, string& s2)
-    {
-        return s1.length() < s2.length();
     }
 };
