b411cb1a8a8b8d6d5c2fba4894a858a04fa7308f
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-08-07 20:09:14 +0530

    Time: 8 ms (99.24%), Space: 8.5 MB (82.41%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
     
-    int f(int ind, vector<int>& arr, int k, vector<int> &dp)
-    {
-        int n = arr.size();
+//     int f(int ind, vector<int>& arr, int k, vector<int> &dp)
+//     {
+//         int n = arr.size();
         
-        if(ind == n) return 0;
+//         if(ind == n) return 0;
         
-        if(dp[ind] != -1) return dp[ind];
-        int maxi = INT_MIN;
-        int maxans = INT_MIN;
-        int len = 0;
+//         if(dp[ind] != -1) return dp[ind];
+//         int maxi = INT_MIN;
+//         int maxans = INT_MIN;
+//         int len = 0;
         
-        for(int j= ind; j<min(n,ind+k);j++)
-        {
-            len++;
-            maxi = max(maxi, arr[j]);
-            int sum = maxi*len + f(j+1,arr,k,dp);
-            maxans= max(maxans, sum);
-        }
-        return dp[ind] = maxans;
-    }
+//         for(int j= ind; j<min(n,ind+k);j++)
+//         {
+//             len++;
+//             maxi = max(maxi, arr[j]);
+//             int sum = maxi*len + f(j+1,arr,k,dp);
+//             maxans= max(maxans, sum);
+//         }
+//         return dp[ind] = maxans;
+//     }
     
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n,-1);
-        return f(0,arr,k,dp);
+        vector<int> dp(n+1,0);
+        for(int ind = n-1;ind>=0;ind--)
+        {
+            int maxi = INT_MIN;
+            int maxans = INT_MIN;
+            int len = 0;
+
+            for(int j= ind; j<min(n,ind+k);j++)
+            {
+                len++;
+                maxi = max(maxi, arr[j]);
+                int sum = maxi*len + dp[j+1];
+                maxans= max(maxans, sum);
+            }
+            dp[ind] = maxans;
+            
+        }
+        
+        return dp[0];
         
     }
 };
