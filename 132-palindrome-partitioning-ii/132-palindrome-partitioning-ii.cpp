5afb5db6f302f2479a3d888276c6b102119782df
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-08-07 19:21:36 +0530

    Time: 634 ms (29.95%), Space: 6.6 MB (79.82%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -13,28 +13,43 @@ public:
         return true;
     }
     
-    int f(int i, int n,string &s,vector<int> &dp )
-    {
-        if(i==n) return 0;
+//     int f(int i, int n,string &s,vector<int> &dp )
+//     {
+//         if(i==n) return 0;
         
-        if(dp[i] != -1) return dp[i];
+//         if(dp[i] != -1) return dp[i];
+//         int mincost = INT_MAX;
+        
+//         for(int j= i;j<n;j++)
+//         {
+//             if(isPaindrome(i,j,s))
+//             {
+//                 int cost = 1+ f(j+1,n,s,dp);
+//                 mincost = min(mincost, cost);
+//             }
+//         }
+//         return dp[i] = mincost;
+//     }
+    
+    int minCut(string s) 
+    {
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        dp[n]= 0;
+        for(int i=n-1;i>=0;i--)
+        {
         int mincost = INT_MAX;
         
         for(int j= i;j<n;j++)
         {
             if(isPaindrome(i,j,s))
             {
-                int cost = 1+ f(j+1,n,s,dp);
+                int cost = 1+ dp[j+1];
                 mincost = min(mincost, cost);
             }
         }
-        return dp[i] = mincost;
-    }
-    
-    int minCut(string s) 
-    {
-        int n = s.size();
-        vector<int> dp(n,-1);
-        return f(0,n,s,dp)-1;
+        dp[i] = mincost;
+        }
+        return dp[0]-1;
     }
 };
