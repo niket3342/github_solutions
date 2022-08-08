a827d431ce92faa2bdbfc15ea43852cb0ba55560
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-08-08 21:33:46 +0530

    Time: 15 ms (83.26%), Space: 10.5 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,1);
-        int maxi=1;
-        for(int i=0;i<n;i++)
+        // int n = nums.size();
+        // vector<int> dp(n,1);
+        // int maxi=1;
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int prev=0;prev<i;prev++)
+        //     {
+        //         if(nums[prev] <nums[i])
+        //         {
+        //             dp[i] = max(dp[i], 1+dp[prev]);
+        //         }
+        //     }
+        //     maxi = max(maxi,dp[i]);
+        // }
+        // return maxi;
+        
+        
+        vector<int> res;
+        for(int i=0;i<nums.size();i++)
         {
-            for(int prev=0;prev<i;prev++)
-            {
-                if(nums[prev] <nums[i])
-                {
-                    dp[i] = max(dp[i], 1+dp[prev]);
-                }
-            }
-            maxi = max(maxi,dp[i]);
+            auto it = std:: lower_bound(res.begin(), res.end(), nums[i]);
+            if(it == res.end()) res.push_back(nums[i]);
+            else *it = nums[i];
         }
-        return maxi;
+        return res.size();
     }
 };
