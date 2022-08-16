aa70fc4cf6662a74167f49bedefaf93a5ccacdf5
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-08-16 12:55:33 +0530

    Time: 67 ms (42.10%), Space: 10.8 MB (15.57%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int n = s.size();
-        vector<int> freq(26,0);
-        for(int i=0;i<n;i++)
-        {
-            freq[s[i]-'a'] += 1;
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(freq[s[i]-'a'] == 1) return i;
-        }
+        unordered_map<char,pair<int, int>> m; //pair if of count and index of that char
         
-        return -1;
+        int idx = s.size();
+        
+        for(int i=0;i<s.size();i++)
+        {
+            m[s[i]].first++;  //increment the count of the character
+            m[s[i]].second = i;  //set index of that char
+            
+        }
+        for(const auto &[c,p]:m)
+        {
+            if(p.first == 1)//if count ==1
+            {
+                idx= min(idx,p.second); //set idx to index of that element
+            }
+        }
+        return idx == s.size() ? -1 : idx; //if there is no such character then return -1
         
     }
 };
