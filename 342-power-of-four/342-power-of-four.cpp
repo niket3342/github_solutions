849be9209f2cd81635badda29f64abb88018c850
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-08-22 20:46:25 +0530

    Time: 3 ms (58.67%), Space: 5.9 MB (71.30%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     bool isPowerOfFour(int num) {
         
-        //BRUTE FORCE APPROACH
-        while(num > 1) {
-            if(num % 4)return false;
-            num /= 4;
-        }
-        return num == 1;
+        //here we check if the num is a power of 2 as any no. which is power of 4 must be a power of 2
+        //by (num & (num - 1)) == 0
+        //and we check if he num, is dividivle by 3 and gives remainder 0
+        
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
         
     }
 };
