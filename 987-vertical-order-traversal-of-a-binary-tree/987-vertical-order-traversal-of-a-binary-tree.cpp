4fcce1ee4073d8edf4c7384bede9dfc660a3b947
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-09-04 10:13:52 +0530

    Time: 6 ms (74.10%), Space: 13.6 MB (49.15%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,31 +13,33 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) 
     {
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0,0}});
+        map<int, map<int, multiset<int>>> nodes;//multiset bcoz tree can have same values (vert, lev, set) 
+        queue<pair<TreeNode*, pair<int, int>>> todo;    //(node, vert,lev)
+        todo.push({root, {0,0}});//pushing the root onto queue
         
         while(!todo.empty())
         {
-            auto p = todo.front();
-            todo.pop();
+            auto p = todo.front();// here p points to node at the front
+            todo.pop();   //popping the frontmost from queue (FIFO)
             TreeNode* temp = p.first;
             
-            int x= p.second.first, y=p.second.second;
-            nodes[x][y].insert(temp->val);
+            int x= p.second.first, y=p.second.second; // x= vertical, y= level
+            nodes[x][y].insert(temp->val); // inserting nodes value onto map
             
             if(temp->left)
             {
-                todo.push({temp->left,{x-1,y+1}});
+                todo.push({temp->left,{x-1,y+1}});//when going to left vectical decreases by 1 and 
+                                                 // level increases by 1
             }
             if(temp->right)
             {
-                todo.push({temp->right,{x+1,y+1}});
+                todo.push({temp->right,{x+1,y+1}});//when going to right vectical increases by 1 and 
+                                                 // level increases by 
             }
         }
         
         vector<vector<int>> ans;
-        for(auto p:nodes)
+        for(auto p:nodes)  // p points to nodes map
         {
             vector<int> col;
             for(auto q:p.second)
