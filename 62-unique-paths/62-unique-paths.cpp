26bbbed2191ee18d0c2b80c657e661c629ac2ba4
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-09-07 08:47:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (75.71%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,19 +1,17 @@
-
-
 class Solution {
 public:
-    int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp)
-    {
-        
-        if(i==m-1 && j==n-1) return 1;//when index reaches the destination
-        if(i>=m || j>=n) return 0; // when pointer goes out of bound
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        else 
-            return dp[i][j] = countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return countPaths(0,0,m,n,dp);
+        //COMBINATORICS APPROACH 
+        int N = m+n-2;
+        int r  = m-1;
+        double res = 1;
+        
+        for(int i=1;i<=r;i++)
+        {
+            res = res*(N-r+i)/i;
+        }
+        
+        return (int) res;
+        
     }
 };
