09ae65aa40e43d3b71c1d7a49df30e1072f31cf9
niket3342 <90603452+niket3342@users.noreply.github.com>
2022-09-11 02:11:45 +0530

    Time: 795 ms (5.03%), Space: 14.6 MB (73.40%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,18 +9,16 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        //BRUTE FORCE APPROACH 
-        
-        unordered_set<ListNode*> st;
-    while(headA != NULL) {
-       st.insert(headA);
-       headA = headA->next;
-    }
-    while(headB != NULL) {
-        if(st.find(headB) != st.end()) return headB;
+        //BRUTE FORCE APPROACH -- Comparing the nodes
+        while(headB != NULL) {
+        ListNode* temp = headA;
+        while(temp != NULL) {
+            //if both nodes are same
+            if(temp == headB) return headB;
+            temp = temp->next;
+        }
         headB = headB->next;
     }
     return NULL;
-        
     }
 };
